0670f48400b203e97ffd179ac3a18ab25089809b
Victor Sint Nicolaas <vicsn@users.noreply.github.com>
2024-05-01 13:18:59 +0200

    Cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,32 @@
-mod bls_elements;
-mod linear_fc;
-mod sphf;
+#[allow(non_snake_case)]
+pub mod bls_elements;
+#[allow(non_snake_case)]
+pub mod linear_fc;
+#[allow(non_snake_case)]
+pub mod sphf;
 
 use bls_elements::BlsElement;
-use blstrs::Gt;
 use blstrs::G1Projective;
 use blstrs::G2Projective;
+use blstrs::Gt;
 use blstrs::Scalar;
 
-use nalgebra::DMatrix;
-use group::Curve;
-use sha3::Shake256;
-use sha3::digest::Update;
-use sha3::digest::ExtendableOutput;
-use sha3::digest::XofReader;
 use bitvec::prelude::Msb0;
 use bitvec::view::BitViewSized;
+use group::Curve;
+use nalgebra::DMatrix;
+use sha3::digest::ExtendableOutput;
+use sha3::digest::Update;
+use sha3::digest::XofReader;
+use sha3::Shake256;
 
-pub fn encrypt_bit(message: bool, ck: (Vec<G1Projective>, Vec<G2Projective>), α: Vec<Scalar>, y: Scalar) -> Result<(bool, DMatrix<BlsElement>, Vec<u8>, (Vec<Scalar>, Scalar)), &'static str> {
+#[allow(non_snake_case)]
+pub fn encrypt_bit(
+    message: bool,
+    ck: (Vec<G1Projective>, Vec<G2Projective>),
+    α: Vec<Scalar>,
+    y: Scalar,
+) -> Result<(bool, DMatrix<BlsElement>, Vec<u8>, (Vec<Scalar>, Scalar)), &'static str> {
     let (cm, d) = linear_fc::commit(ck.clone(), α.clone());
 
     let Γ = sphf::gen_Γ_linear_fc(cm, ck.clone());
@@ -34,17 +43,23 @@ pub fn encrypt_bit(message: bool, ck: (Vec<G1Projective>, Vec<G2Projective>), α
     Ok((ciphertext, projected_hash_key, r, d))
 }
 
-pub fn decrypt_bit(ciphertext: bool, ck: (Vec<G1Projective>, Vec<G2Projective>), α: Vec<Scalar>, β: Vec<Scalar>, projected_hash_key: DMatrix<BlsElement>, r: Vec<u8>, d: (Vec<Scalar>, Scalar)) -> bool {
+#[allow(non_snake_case)]
+pub fn decrypt_bit(
+    ciphertext: bool,
+    ck: (Vec<G1Projective>, Vec<G2Projective>),
+    α: Vec<Scalar>,
+    β: Vec<Scalar>,
+    projected_hash_key: DMatrix<BlsElement>,
+    r: Vec<u8>,
+    d: (Vec<Scalar>, Scalar),
+) -> bool {
     let opening = linear_fc::open(ck.clone(), d, β.clone());
 
     // diverging from the WE_FC paper's implementation
-    let ADJUSTMENT_3 = ck.0[0].to_affine()*β[1]*α[0]; // G^(u*α1*β2)
+    let ADJUSTMENT_3 = ck.0[0].to_affine() * β[1] * α[0]; // G^(u*α1*β2)
 
-    let λ = sphf::gen_λ_linear_fc(
-        ck.1[1]*β[0] + ck.1[0]*β[1],
-        opening - ADJUSTMENT_3,
-    );
-    let res = projected_hash_key[0]*λ[0] - projected_hash_key[1]*λ[1];
+    let λ = sphf::gen_λ_linear_fc(ck.1[1] * β[0] + ck.1[0] * β[1], opening - ADJUSTMENT_3);
+    let res = projected_hash_key[0] * λ[0] - projected_hash_key[1] * λ[1];
     if let BlsElement::Gt(projected_hash) = res {
         let plaintext = encrypt_message(ciphertext, projected_hash, r.clone());
         plaintext
@@ -76,21 +91,20 @@ fn encrypt_message(message: bool, hashkey: Gt, rand_bytes: Vec<u8>) -> bool {
 mod tests {
     use super::*;
 
-    fn encryption_decryption(should_succeed: bool, message: bool) -> bool {
+    fn encryption_decryption(message: bool) -> bool {
         let witness_length = 2;
-        let (ck, α, β, mut y) = linear_fc::setup_random(witness_length);
-        let (ciphertext, projected_hash_key, r, d) = encrypt_bit(message, ck.clone(), α.clone(), y).unwrap();
-        if !should_succeed {
-            y = y*y;
-        }
+        let (ck, α, β, y) = linear_fc::setup_random(witness_length);
+        let (ciphertext, projected_hash_key, r, d) =
+            encrypt_bit(message, ck.clone(), α.clone(), y).unwrap();
         decrypt_bit(ciphertext, ck, α, β, projected_hash_key, r, d)
     }
 
     #[test]
     fn encryption_decryption_success() {
-        let message = true;
-        for _ in 0..128 { // we're only encrypting a bit, which can be easily randomly correct, so to properly test we should run this often
-            assert_eq!(encryption_decryption(true, message), message);            
+        for _ in 0..128 {
+            // we're only encrypting a bit, which can be easily randomly correct, so to properly test we should run this often
+            let message = rand::random::<bool>();
+            assert_eq!(encryption_decryption(message), message);
         }
     }
 }
